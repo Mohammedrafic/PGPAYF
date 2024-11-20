2b46f9d4be8a6c1d52c96c5ba9f34d6d2808a125
CIPL1318 <CIPL1318@CIPL1318DBA>
2024-11-20 19:56:57 +0530

    Add hostels

--- src/app/Main/add-hostels/add-hostels.component.ts
+++ src/app/Main/add-hostels/add-hostels.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { ServiceService } from './service/service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-hostels',
@@ -13,8 +15,9 @@ export class AddHostelsComponent implements OnInit {
 
   selectedFiles: any[] = [];
   HostelDetails: any;
+  UserID: any;
 
-  constructor(private fb: FormBuilder,private toastr: ToastrService) {
+  constructor(private fb: FormBuilder,private toastr: ToastrService, private service: ServiceService, private router: Router) {
   }
 
   ngOnInit(): void {
@@ -28,10 +31,15 @@ export class AddHostelsComponent implements OnInit {
       ownerName: ['', Validators.required],
       HostelPhotos: [[]] 
     });
+
+    const userId = localStorage.getItem('userId');
+  
+    if (userId !== null) {
+      this.UserID = parseInt(userId, 10);
+    }
   }
 
   onFileSelected(event: any): void {
-    debugger;
     const files: FileList = event.target.files;
     if (files.length > 0) {
       const selectedFiles: File[] = [];
@@ -43,14 +51,53 @@ export class AddHostelsComponent implements OnInit {
   }
 
   Submit(){
+    debugger;
     if (this.HostelDetailsForm.valid) {
       this.HostelDetails = this.HostelDetailsForm.value;
       if(this.HostelDetails){
         this.HostelDetails.HostelPhotos = this.selectedFiles;
+        this.service.AddHostelDetails(this.formValues()).subscribe((res: any) => {
+          if(res.content){
+            this.toastr.success('Added Successfully','Success')
+            this.router.navigate(['main/dashboard'])
+          }else{
+            this.toastr.error(res.message,'Error')
+          }
+        });
       }
     } else {
       this.toastr.error('Invalid HostelDetails Form!', 'Error')
     }
     console.log(this.HostelDetailsForm);
+  }
+
+  formValues() {
+    const formData = new FormData();
+    const UserDetails: any = {
+      UserID: this.UserID,
+      HostelName: this.HostelDetails?.HostelName || '',
+      HostalAddress: this.HostelDetails?.HostalAddress || '',
+      NoOfRooms: this.HostelDetails?.NoOfRooms || 0,
+      MinimumRent: this.HostelDetails?.minrent || 0,
+      MaximunRent: this.HostelDetails?.maxrent || 0,
+      ContactNumber: this.HostelDetails?.contactno || 0,
+      OwnerName: this.HostelDetails?.ownerName || '',
+      HostalPhotosPath: this.HostelDetails?.HostelPhotos || []
+    };
+  
+    formData.append("UserID", UserDetails.UserID);
+    formData.append("HostelName", UserDetails.HostelName);
+    formData.append("HostalAddress", UserDetails.HostalAddress);
+    formData.append("NoOfRooms", UserDetails.NoOfRooms.toString());
+    formData.append("MinimumRent", UserDetails.MinimumRent.toString());
+    formData.append("MaximunRent", UserDetails.MaximunRent.toString());
+    formData.append("ContactNumber", UserDetails.ContactNumber.toString());
+    formData.append("OwnerName", UserDetails.OwnerName);
+    
+    // Append each file in HostalPhotosPath array
+    for (const file of UserDetails.HostalPhotosPath) {
+      formData.append("HostelDetails.HostalPhotosPath", file, file.name);
+    }
+    return formData;
   }
 }
---
+++ src/app/Main/add-hostels/service/service.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+  constructor(private http: HttpClient) { }
+
+  AddHostelDetails(HostelDetails: any) {
+    return this.http.post<any>(`${environment.BaseUrl}AddHostelDetails`, HostelDetails);
+  }
+}
